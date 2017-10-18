18b5476d6d5d6152b24784aa855fb5b98009ede3
Evan Tichenor <evan.tichenor@gmail.com>
2017-10-17 22:02:58 -0400

    Added everything.
    This is a pretty simple game, dont expect anything major.

---
+++ com/evan/tichenor/tron/generic/Value.java
@@ -0,0 +1,14 @@
+package com.evan.tichenor.tron.generic;
+
+/**
+ * @author Evan Tichenor (evan.tichenor@gmail.com)
+ * @version 1.0, 10/16/2017
+ */
+public class Value<T> {
+
+    public T value;
+
+    public Value(T value) {
+        this.value = value;
+    }
+}
